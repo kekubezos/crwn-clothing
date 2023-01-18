dcbd2cec2a8e22a786398ec09d79ae9cf050d421
kekubezos <keknorgbey@yahoo.com>
2023-01-18 16:50:15 +0000

    Added Redux to header and app components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route, Redirect} from 'react-router-dom';
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 import { connect } from 'react-redux'
 import {setCurrentUser} from './redux/user/user.actions'
@@ -46,15 +46,21 @@ class App extends React.Component{
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route  exact path='/shop' component={ShopPage}/>
-                    <Route exact path='/sign-in' component={SignInAndSignUp}/>
+                    {/*Redirects to homepage after signing in.*/}
+                    <Route exact path='/sign-in' render={()=> this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)} />
                 </Switch>
             </div>
         )
     }
 }
 
+const mapStateToProps =({user})=>({
+    currentUser: user.currentUser
+
+})
+
 const mapDispatchToProps= (dispatch)=>({
     setCurrentUser: (user)=> dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
